061ab1c169964974187f4cc948469d92898c3b39
kbailey4444 <kbailey4444@gmail.com>
2020-05-08 18:13:41 -0400

    routes component

--- mta-bus-map/src/app/app.module.ts
+++ mta-bus-map/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { RoutesComponent } from './routes/routes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    RoutesComponent
   ],
   imports: [
     BrowserModule,
